4616990f045c8ad9ea7ec26572a137639e4afc74
S.Drwsh <darwish.saja@gmail.com>
2025-09-28 21:32:08 +0200

    fix: yarn 4 npm audit

--- src/filter-audit.js
+++ src/filter-audit.js
@@ -16,56 +16,58 @@ args.forEach(arg => {
 });
 
 exec('yarn --version', (versionError, versionStdout, versionStderr) => {
-   if (versionError) {
-       console.error(`Error fetching Yarn version: ${versionError?.message}`);
-       return;
-   }
-   if (versionStderr) {
-       console.error(`Yarn version stderr: ${versionStderr}`);
-   }
-   const yarnVersion = versionStdout.trim();
-   console.log(`Yarn version: ${yarnVersion}`);
+    if (versionError) {
+        console.error(`Error fetching Yarn version: ${versionError?.message}`);
+        return;
+    }
+    if (versionStderr) {
+        console.error(`Yarn version stderr: ${versionStderr}`);
+    }
+    const yarnVersion = versionStdout.trim();
+    console.log(`Yarn version: ${yarnVersion}`);
 
-   const flagsString = additionalFlags.join(' ');
+    const flagsString = additionalFlags.join(' ');
 
-   exec(`yarn audit --json ${flagsString}`, (error, stdout, stderr) => {
     
+    const auditCommand = yarnVersion.startsWith('4.')
+        ? `yarn npm audit --json ${flagsString}`
+        : `yarn audit --json ${flagsString}`;
 
-       if (stderr) {
-           console.error(`stderr: ${stderr}`);
-       }
+    exec(auditCommand, (error, stdout, stderr) => {
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
 
+        if (error && !stdout) {
+            console.error(`exec error message: ${error?.message}`);
+            console.error(`exec error stack: ${error?.stack}`);
+            process.exit(1);
+        }
 
-       if (error && !stdout) {
-           console.error(`exec error message: ${error?.message}`);
-           console.error(`exec error stack: ${error?.stack}`);
-           process.exit(1);
-       }
+        const lines = stdout.trim().split('\n');
+        let hasAdvisories = false;
+        let advisories = [];
 
-       const lines = stdout.trim().split('\n');
-       let hasAdvisories = false;
-       let advisories = [];
-
-       if (yarnVersion.startsWith('1.')) {
-           // Yarn 1 script
-           lines.forEach((line) => {
-                if(line){
+        if (yarnVersion.startsWith('1.')) {
+            // Yarn 1 script
+            lines.forEach((line) => {
+                if (line) {
                     const json = JSON.parse(line);
 
                     const advisoryId = json?.data?.advisory?.github_advisory_id;
                     if (
-                        advisoryId && !advisoryToFilter.some((advisory) => advisoryId?.includes(advisory))
+                        advisoryId &&
+                        !advisoryToFilter.some((advisory) => advisoryId?.includes(advisory))
                     ) {
                         hasAdvisories = true;
                         advisories.push(json?.data?.advisory);
                     }
                 }
-                    
-           });
-       } else if (yarnVersion.startsWith('4.')) {
-           // Yarn 4 script
-           lines.forEach((line) => {
-                if(line){
+            });
+        } else if (yarnVersion.startsWith('4.')) {
+            // Yarn 4 script
+            lines.forEach((line) => {
+                if (line) {
                     const json = JSON.parse(line);
                     const advisoryUrl = json?.children?.URL;
 
@@ -76,22 +78,27 @@ exec('yarn --version', (versionError, versionStdout, versionStderr) => {
                         console.error(`Advisory: ${json?.value}`);
                         console.error(`Details: ${JSON.stringify(json?.children, null, 2)}`);
                     }
-                }  
-           });
-       } else {
-           console.error(`Unsupported Yarn version: ${yarnVersion}`);
-           process.exit(1);
-       }
+                }
+            });
+        } else {
+            console.error(`Unsupported Yarn version: ${yarnVersion}`);
+            process.exit(1);
+        }
 
-       if (hasAdvisories) {
-           if(advisories.length > 0){
-               console.table(advisories, ['module_name', 'severity', 'vulnerable_versions', 'patched_versions', 'github_advisory_id']);
-           }
-           process.exit(1);
-       } else {
-           console.log('No relevant advisories found.');
-           process.exit(0);
-       }
-   });
+        if (hasAdvisories) {
+            if (advisories.length > 0) {
+                console.table(advisories, [
+                    'module_name',
+                    'severity',
+                    'vulnerable_versions',
+                    'patched_versions',
+                    'github_advisory_id',
+                ]);
+            }
+            process.exit(1);
+        } else {
+            console.log('No relevant advisories found.');
+            process.exit(0);
+        }
+    });
 });
-
